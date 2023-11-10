d0f8878dcec42e7f14f81f162ad3eb6964d63f1d
Martin Wheeler <martin@martinwheeler.com.au>
2023-11-10 16:58:01 +1000

    build: release v1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## v1.0.0 - Fri Nov 10 2023 16:58:01 GMT+1000 (Australian Eastern Standard Time)
+
+### Added
+    - Merged feature/branches
+### Fixed
+    - Merged bugfix/branches
+    - Merged hotfix/branches
+
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { input } from "@inquirer/prompts";
 import chalk from "chalk";
 import util from "node:util";
 import { exec } from "node:child_process";
+import cliSpinners from "cli-spinners";
+import ora from "ora";
 
 const asyncExec = util.promisify(exec);
 
@@ -20,18 +22,51 @@ console.log(
 
 const version = await input({
   message: "What version would you like to assign to this release?",
+  default: "1.0.0",
 });
 
 const issueTag = await input({
   message:
     "What issue tag do you want to scan for? (this helps create the changelog)",
+  default: "SR",
 });
+
+const { stdout: changelogCommits, stderr: commitCheckError } = await asyncExec(
+  `git log --oneline | grep -Eo "${issueTag}-[0-9]+"`
+);
+
+if (
+  changelogCommits &&
+  SOURCE_BRANCH === "dev" &&
+  TARGET_BRANCH === "staging"
+) {
+  const timestamp = new Date().toString();
+
+  // NOTE: No indents for the message otherwise it is reflected in the CHANGELOG.md file too
+  const newChanelogEntry = `## v${version} - ${timestamp}
+
+### Added
+    - Merged feature/branches
+### Fixed
+    - Merged bugfix/branches
+    - Merged hotfix/branches
+`;
+
+  const { stdout: appendingChangelog, stderr: changelogError } =
+    await asyncExec(`echo "${newChanelogEntry}" > CHANGELOG.md`);
+}
 
 console.log({
   version,
-  issueTag: `${issueTag}-[0-9]+`,
+  issueTag,
+  changelogCommits,
 });
 
-const { stdout, stderr } = await asyncExec("git log");
-console.log("stdout:", stdout);
-console.error("stderr:", stderr);
+await asyncExec(`git add -A`);
+await asyncExec(`git commit -m 'build: release v${version}'`);
+
+const { stdout: tag, stderr: tagError } = await asyncExec(
+  `git tag -a ${version} -m 'build: release v${version}'`
+);
+
+// spinner.stop();
